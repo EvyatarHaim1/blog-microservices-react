a4102889b5aeae1c3cbdc111c881336f85ceca40
Evyatar Haim <evyataros1@gmail.com>
2021-10-02 17:58:50 +0300

    a

--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { randomBytes } = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+
 const posts = {};
 
 app.get('/posts', (req, res) => {
