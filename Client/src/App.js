0502f5adec96ee29f73e4c553e6a4031f8b4bb25
Evyatar Haim <evyataros1@gmail.com>
2021-10-02 18:06:10 +0300

    f

---
+++ Client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PostCreate from './PostCreate';
+
+export default function App() {
+    return (
+        <div className="container">
+            <h1> Create Post:</h1>
+            <PostCreate />
+        </div>
+    )
+}
---
+++ Client/src/PostCreate.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function PostCreate() {
+
+    const [title, setTitle] = useState('');
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        await axios.post('http://localhost:4000/posts', {
+            title
+        });
+
+        setTitle('');
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label> Title </label>
+                    <input
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                        className="form-control" type="text" />
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
