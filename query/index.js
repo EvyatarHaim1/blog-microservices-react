62630e0e3ec13230f20678f9d022dd4c1b294e57
Evyatar Haim <evyataros1@gmail.com>
2021-10-04 08:10:09 +0300

    adding code to query service

--- query/index.js
+++ query/index.js
@@ -22,10 +22,22 @@ app.post('/events', (req, res) => {
     }
 
     if (type === "CommentCreated") {
-        const { id, content, postId } = data;
+        const { id, content, postId, status } = data;
 
         const post = posts[postId];
         post.comments.push({ id, content, status });
+    }
+
+    if (type === "CommentUpdated") {
+        const { id, content, postId, status } = data;
+
+        const post = posts[postId];
+        const comment = post.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+        comment.content = content;
     }
 
     console.log(posts);
