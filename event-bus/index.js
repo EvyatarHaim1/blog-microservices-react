3861a6ce4f93a331585d631bf8725ded45fda187
Evyatar Haim <evyataros1@gmail.com>
2021-10-04 08:38:20 +0300

    finish the first app of micro-services

--- event-bus/index.js
+++ event-bus/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 
-const app = express();
-app.use(bodyParser.json());
+const events = [];
 
+const app = express();
 app.post('/events', (req, res) => {
     const event = req.body;
+
+    events.push(event);
 
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
@@ -14,6 +16,10 @@ app.post('/events', (req, res) => {
     axios.post('http://localhost:4003/events', event);
 
     res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 });
 
 app.listen(4005, () => {
